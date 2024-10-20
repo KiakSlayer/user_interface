4f764c5eeaa8b45e8cbc354ce9bed9bb80716ca3
PeakPattanun <peaksg31@gmail.com>
2024-10-20 16:17:22 +0700

    Merged with meno-deco, audio and profile feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Menu from "./pages/Menu.jsx";
-import Start from "./pages/SelectMap.jsx";
+// import Start from "./pages/SelectMap.jsx";
 import Profile from "./pages/Profile/Profile.jsx";
 import Setting from "./pages/Settings/Settings.jsx";
 import GamePlay from "./pages/Gameplay.jsx";
-import Cutscene from "./pages/Cutscene.jsx";
-import Role from "./pages/RoleAssignment.jsx";
-import sound from "./assets/forest_sounds.mp3"
+// import Cutscene from "./pages/Cutscene.jsx";
+// import Role from "./pages/RoleAssignment.jsx";
+// import sound from "./assets/forest_sounds.mp3"
 
 const App = () => {
   return (
@@ -15,6 +15,8 @@ const App = () => {
       <Routes>
         {/* Route for the Menu page */}
         <Route path="/" element={<Menu />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/profile" element={<Profile />} />
         
         {/* Route for the GamePlay page at the "/start" path */}
         <Route path="/start" element={<GamePlay />} />
