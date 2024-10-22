3355c2c909fa961b979856d27ac8f73986293591
Tuwadon Wang <kiak2546@gmail.com>
2024-10-22 18:38:11 +0700

    Basic game demo logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import './App.css';
-import { AudioProvider } from './context/AudioContext';
-import { ThemeProvider } from './context/ThemeContext';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AudioProvider> 
-
-      <ThemeProvider>
-      <App />
-      
-      </ThemeProvider>  
-    </AudioProvider>
-
+    <App />
   </StrictMode>,
 )
-
-//audioProvider--> Manages audio playback globally
-//ThemeProvider--> Manages dark and light theme globally
